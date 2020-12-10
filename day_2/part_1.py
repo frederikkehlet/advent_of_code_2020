22a5a0b696086aaef9578735ba6ef92ffab9474a
Frederik Kehlet <frederik.kehlet@outlook.dk>
2020-12-10 11:31:09 +0100

    solved part 1 day 2

---
+++ day_2/part_1.py
@@ -0,0 +1,30 @@
+# convert txt list to int list
+input = open('input.txt', 'r')
+lines = input.readlines()
+
+for i in range(len(lines)): 
+    lines[i] = lines[i].strip()
+
+def solve():
+    n_valid = 0
+
+    for line in lines:
+        split = line.split(': ')
+        policy = split[0]
+        pwd = split[1]
+        policy_split = policy.split(' ')
+        min_max = policy_split[0].split('-')
+        char = policy_split[1]
+        mn = int(min_max[0])
+        mx = int(min_max[1])
+        counter = pwd.count(char)
+
+        if (mn <= counter <= mx):
+            n_valid += 1
+
+    return n_valid
+
+output = solve()
+print(output)
+
+
