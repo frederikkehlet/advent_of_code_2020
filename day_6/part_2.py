063c490f216d5f7fc9c0c28b92cda95dd7723531
Frederik Kehlet <frederik.kehlet@outlook.dk>
2020-12-14 20:11:46 +0100

    solved day 6 part 2

---
+++ day_6/part_2.py
@@ -0,0 +1,21 @@
+groups = []
+
+with open('input.txt', 'r') as f:
+    lines = f.read()
+    groups = lines.split("\n\n")
+    groups = [group.replace('\n', ' ') for group in groups]
+
+def solve():
+    total_count = 0
+
+    for group in groups:
+        sets = [set(x) for x in list(group.split(' '))]
+
+        answers_all = set.intersection(*sets)
+
+        total_count += len(answers_all)
+    
+    return total_count
+
+output = solve()
+print(output)
