c17151b581325764e4898fd1ae24513d5f88d9cf
Frederik Kehlet <frederik.kehlet@outlook.dk>
2021-05-23 16:12:37 +0200

    solved day 15 part 1

---
+++ day_15/part_1.py
@@ -0,0 +1,28 @@
+input = open('input.txt', 'r')
+lines = input.readlines()
+
+def solve():
+    numbers = [int(num) for num in list(reversed(lines[0].split(',')))]
+    turn = len(numbers) + 1
+    while turn < 2021:
+        lastSpokenNumber = numbers[0]
+        previousNumbers = numbers[1:]
+        
+        i = 0
+        latestTurn = 0
+        spokenPreviously = False
+        while i < len(previousNumbers):        
+            if lastSpokenNumber == int(previousNumbers[i]):
+                spokenPreviously = True
+                latestTurn = i
+                break
+            i += 1
+        
+        if not spokenPreviously: numbers.insert(0,0)
+        else: numbers.insert(0,latestTurn + 1)
+
+        turn += 1
+    return numbers[0]
+
+answer = solve()
+print(answer)
