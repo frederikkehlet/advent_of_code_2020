fbd2e13a7791f72170a584b153470e8bad9ad4d7
Frederik Kehlet <frederik.kehlet@outlook.dk>
2021-05-23 16:14:40 +0200

    syntactic changes

--- day_15/part_1.py
+++ day_15/part_1.py
@@ -4,7 +4,7 @@ lines = input.readlines()
 def solve():
     numbers = [int(num) for num in list(reversed(lines[0].split(',')))]
     turn = len(numbers) + 1
-    while turn < 2021:
+    while turn <= 2020:
         lastSpokenNumber = numbers[0]
         previousNumbers = numbers[1:]
         
@@ -20,8 +20,8 @@ def solve():
         
         if not spokenPreviously: numbers.insert(0,0)
         else: numbers.insert(0,latestTurn + 1)
-
         turn += 1
+        
     return numbers[0]
 
 answer = solve()
